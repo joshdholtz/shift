23d968442ee73a96942f01ee8c2d6d30feefea9f
Josh <joshdholtz.junk@gmail.com>
2011-10-06 22:59:48 -0500

    Committing update top nav bar and added under construction to empty pages

--- routes/website.rb
+++ routes/website.rb
@@ -9,9 +9,34 @@ module Route
 				redirect "/dashboard"
 			end
 
+			@page = "index"
 			erb :index
 		end
 
+		get '/features' do
+			@page = "features"
+			erb :features
+		end
+
+		get '/starthere' do
+			@page = "starthere"
+			erb :starthere
+		end
+
+		get '/doc' do
+			@page = "doc"
+			erb :doc
+		end
+
+		get '/faq' do
+			@page = "faq"
+			erb :faq
+		end
+
+		get '/about' do
+			@page = "about"
+			erb :about
+		end
 
 	end
 end
